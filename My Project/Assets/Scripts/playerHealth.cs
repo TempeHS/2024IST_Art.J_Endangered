0199962f3308df696968df72f87cd948e1a9998e
ISTArtJ <art.jacona@education.nsw.gov.au>
2024-06-11 11:27:16 +1000

    remove current HP bar, move towards new HP bar code

---
+++ My Project/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
