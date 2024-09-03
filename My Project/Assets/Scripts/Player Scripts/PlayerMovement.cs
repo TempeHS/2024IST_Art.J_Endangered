805718638bb4890f12474d9af35489ac5562a4d1
ISTArtJ <art.jacona@education.nsw.gov.au>
2024-09-03 11:55:35 +1000

    More move code, left off at 4:44

--- My Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ My Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    public PlayerMovementStats MoveStats;
+    [SerializeField] private Collider2D _feetColl;
+    [SerializeField] private Collider2D _bodyColl;
+
+    private Rigidbody2D _rb;
+
+    //movement vars
+    private Vector2 _moveVelocity;
+    private bool _isFacingRight;
+
+    //collision check vars
+    private RayCastHit2D _groundHit;
+    private RayCastHit2D _headHit;
+    private bool _isGrounded;
+    private bool _bumpedHead;
+
+    private void Awake()
     {
-        
+        _isFacingRight = true;
+
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Movement
+
+    private void Move(float acceleration, float deceleration, Vector2 moveInput)
     {
-        
+        if (moveInput != Vector2.zero)
+        {
+            //check if he needs to turn
+
+            Vector2 targetVelocity = Vector2.zero;
+            if (InputManager.WalkIsHeld)
+        }
     }
+
+    #endregion
 }
--- My Project/Assets/Scripts/Player Scripts/PlayerMovementStats.cs
+++ My Project/Assets/Scripts/Player Scripts/PlayerMovementStats.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementStats : MonoBehaviour
+[CreateAssetMenu(menuName ="Player Movement")]
+public class PlayerMovementStats : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Walk")]
+    [Range(1f, 100f)] public float MaxWalkSpeed = 20f;
+    [Range(0.25f, 50f)] public float GroundAcceleration = 5f;
+    [Range(0.25f, 50f)] public float GroundDeceleration = 20f;
+    [Range(0.25f, 50f)] public float AirAcceleration = 5f;
+    [Range(0.25f, 50f)] public float AirDeceleration = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Grounded/Collision Checks")]
+    public LayerMask GroundLayer;
+    public float GroundDetectionRayLength = 0.02f;
+    public float HeadDecetionRayLength = 0.02f;
+    [Range(0f, 1f)] public float HeadWidth = 0.75f;
 }
