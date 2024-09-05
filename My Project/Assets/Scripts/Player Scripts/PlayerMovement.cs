8cfb4f8e1a019ab3e483b39fde9a1a2425c9a362
ISTArtJ <art.jacona@education.nsw.gov.au>
2024-09-05 11:55:26 +1000

    More movement left off at 7:35

--- My Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ My Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -16,8 +16,8 @@ public class PlayerMovement : MonoBehaviour
     private bool _isFacingRight;
 
     //collision check vars
-    private RayCastHit2D _groundHit;
-    private RayCastHit2D _headHit;
+    private RaycastHit2D _groundHit;
+    private RaycastHit2D _headHit;
     private bool _isGrounded;
     private bool _bumpedHead;
 
@@ -28,17 +28,90 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        CollisionChecks();
+
+        if (_isGrounded)
+        {
+            Move(MoveStats.GroundAcceleration, MoveStats.GroundDeceleration, InputManager.Movement);
+        }
+        else
+        {
+            Move(MoveStats.AirAcceleration, MoveStats.AirDeceleration, InputManager.Movement);
+        }
+    }
+
     #region Movement
 
     private void Move(float acceleration, float deceleration, Vector2 moveInput)
     {
         if (moveInput != Vector2.zero)
         {
-            //check if he needs to turn
+            TurnCheck(moveInput);
 
             Vector2 targetVelocity = Vector2.zero;
-            if (InputManager.WalkIsHeld)
+            targetVelocity = new Vector2(moveInput.x, 0f) * MoveStats.MaxWalkSpeed;
+
+            _moveVelocity = Vector2.Lerp(_moveVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+            _rb.velocity = new Vector2(_moveVelocity.x, _rb.velocity.y);
         }
+
+        else if (moveInput == Vector2.zero)
+        {
+            _moveVelocity = Vector2.Lerp(_moveVelocity, Vector2.zero, deceleration * Time.fixedDeltaTime);
+            _rb.velocity = new Vector2(_moveVelocity.x, _rb.velocity.y);
+        }
+
+    }
+
+    private void TurnCheck(Vector2 moveInput)
+    {
+        if (_isFacingRight && moveInput.x <0)
+        {
+            Turn(false);
+        }
+        
+        else if (!_isFacingRight && moveInput.x > 0)
+        {
+            Turn(true);
+        }
+    }
+
+    private void Turn(bool turnRight)
+    {
+        if (turnRight)
+        {
+            _isFacingRight = true;
+            transform.Rotate(0f, 180f, 0f);
+        }
+        else
+        {
+            _isFacingRight = false;
+            transform.Rotate(0f, -180f, 0f);
+        }
+    }
+
+    #endregion
+
+    #region Collision Checks
+
+    private void IsGrounded()
+    {
+        Vector2 boxCastOrigin = new Vector2(_feetColl.bounds.center.x, _feetColl.bounds.min.y);
+        Vector2 boxCastSize = new Vector2(_feetColl.bounds.size.x, MoveStats.GroundDetectionRayLength);
+
+        _groundHit = Physics2D.BoxCast(boxCastOrigin, boxCastSize, 0f, Vector2.down, MoveStats.GroundDetectionRayLength, MoveStats.GroundLayer);
+        if (_groundHit.collider != null)
+        {
+            _isGrounded = true;
+        }
+        else {_isGrounded = false; }
+    }
+
+    private void CollisionChecks()
+    {
+        IsGrounded();
     }
 
     #endregion
