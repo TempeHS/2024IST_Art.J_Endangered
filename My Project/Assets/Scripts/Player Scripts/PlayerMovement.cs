e224910447ab52a0fdf8e616e1c501b37de33522
ISTArtJ <art.jacona@education.nsw.gov.au>
2024-09-24 09:22:18 +1000

    Revert "edit"
    This reverts commit 2eecd73ad6e896290112f40a0b1a63573e9977eb.

--- My Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ My Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-<<<<<<< HEAD
     [Header("References")]
     public PlayerMovementStats MoveStats;
     [SerializeField] private Collider2D _feetColl;
@@ -54,83 +53,6 @@ public class PlayerMovement : MonoBehaviour
     {
         CountTimers();
         JumpChecks();
-=======
-    private float horizontal;
-    private float speed = 12f;
-    private float jumpingPower = 20f;
-    private bool isFacingRight = true;
-
-    private float coyoteTime = 0.1f;
-    private float coyoteTimeCounter;
-
-    private float jumpBufferTime = 0.05f;
-    private float jumpBufferCounter;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-
-    // Update is called once per frame
-    private void Update()
-    {
-
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (IsGrounded())
-        {
-            coyoteTimeCounter = coyoteTime;
-        }
-        else
-        {
-            coyoteTimeCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jumpBufferCounter = jumpBufferTime;
-        }
-        else
-        {
-            jumpBufferCounter -= Time.deltaTime;
-        }
-
-        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-
-            jumpBufferCounter = 0f;
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-
-            coyoteTimeCounter = 0f;
-        }
-
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-    
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
->>>>>>> parent of 9f81934e (Began Work on Move Revamp, left off from stats)
     }
 
     private void FixedUpdate()
